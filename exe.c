37f88ea85fbab24afcd7f9a2219d3c5348416d1d
IrisTheSnail <salwa999adlouni@gmail.com>
2022-11-02 13:23:46 +0100

    yeah C mazal kayn

--- exe.c
+++ exe.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main() {
-	char* c = "\nHello world";
-	printf("%s", c);
+int main(int argc, char * argv[]) {
+	printf("MOIS\t:\t%d\n", atoi(argv[1]));
 	return 0;
 }
