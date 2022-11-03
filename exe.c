c69cc43a08057cebadbb5dee9e17fb76176352a0
IrisTheSnail <salwa999adlouni@gmail.com>
2022-11-03 14:41:04 +0100

    paint the terminal

--- exe.c
+++ exe.c
@@ -1,7 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include "chadstr.h"
 
 int main(int argc, char * argv[]) {
-	printf("MOIS\t:\t%d\n", atoi(argv[1]));
+
+	system("printf '\e[48;5;022m\033[39m'");
+	//chadstr On_Green = str("\033[42m\]");
+	//SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),BACKGROUND_INTENSITY|BACKGROUND_RED);
+	chadstr coo = str(str("MOIS:"), str(atoi(argv[1]), "\n"), str("\n"));
+	printf(str(*coo));
+
+	for (int i = 1; i <= 30; i++){
+      	printf("%d|", i);
+      	
+    }
+    system("pwd");
+	printf("\n");
 	return 0;
 }
